436639f42a70e7bd84833fe576113f612669da1f
Etokapa <31154606+Etokapa@users.noreply.github.com>
2025-12-30 15:40:38 -0600

    Add files via upload

---
+++ Tools/Extract Text.bat
@@ -0,0 +1,22 @@
+::Extract to msgout01.msg.txt
+duomsg.exe msgout00.msg
+duomsg.exe msgout01.msg
+duomsg.exe msgout02.msg
+duomsg.exe msgout03.msg
+duomsg.exe msgout04.msg
+duomsg.exe msgout05.msg
+duomsg.exe msgout06.msg
+duomsg.exe msgout07.msg
+duomsg.exe msgout08.msg
+duomsg.exe msgout09.msg
+
+ren msgout00.msg.txt msgout00.txt
+ren msgout01.msg.txt msgout01.txt
+ren msgout02.msg.txt msgout02.txt
+ren msgout03.msg.txt msgout03.txt
+ren msgout04.msg.txt msgout04.txt
+ren msgout05.msg.txt msgout05.txt
+ren msgout06.msg.txt msgout06.txt
+ren msgout07.msg.txt msgout07.txt
+ren msgout08.msg.txt msgout08.txt
+ren msgout09.msg.txt msgout09.txt
